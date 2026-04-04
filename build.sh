3f80cacb19c99c31fb90beb9ed28a1677f81b309
Rishee Sharma <114845811+rishee10@users.noreply.github.com>
2026-04-04 15:02:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
+
+echo "Installing dependencies..."
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+echo "Making migrations..."
+python manage.py makemigrations
+
+echo "Applying migrations..."
 python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
